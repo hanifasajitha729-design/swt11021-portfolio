60d48020a65b5f46ef2054b89458b9487563db07
hanifasajitha729-design <hanifasajitha729@gmail.com>
2026-05-24 15:56:54 +0530

    week01:hello world,pipeline demo,greet,escape

---
+++ week01/escape.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name:\tHanifa sajitha\n");
+    printf("course:\tSWT11021\n");
+    printf("Path:\tC:\\Users\\student\n\n");
+    printf("she said:\"hello!\"\n");
+
+    return 0;
+}
---
+++ week01/greet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Good morning, wecome to SEUSL world!......\n");
+    printf("this is the first C programming language in our class..\n");
+    printf("Sajitha Hanifa\n");
+    printf("SEU/IS/24/ICT/092\n");
+    printf("DICT\n");
+
+    return 0;
+}
